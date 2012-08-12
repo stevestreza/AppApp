63b13b277120a489ff255a97a4053c0f4e43ade6
brandon <brandon@redbook.local>
2012-08-12 16:54:49 -0700

    working on the post detail / replies functionality.

---
+++ AppApp/ANPostDetailHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  ANPostDetailHeaderView.h
+//  AppApp
+//
+//  Created by brandon on 8/12/12.
+//  Copyright (c) 2012 Sneakyness. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANPostDetailHeaderView : UIView
+
+@end
