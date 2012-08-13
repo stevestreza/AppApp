dc8fcc9cc333a5cd0c2f67ba2b810c335be1d529
brandon <brandon@redbook.local>
2012-08-13 00:15:13 -0700

    added refresh when returning to foreground for all tabs.

--- AppApp/ANAppDelegate.m
+++ AppApp/ANAppDelegate.m
@@ -98,6 +98,8 @@ static ANAppDelegate *sharedInstance = nil;
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    NSArray *controllers = self.sideMenuController.navigationArray;
+    [controllers makeObjectsPerformSelector:@selector(refresh)];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
